18be997ebfb66c061e623bccaa8cabb63325fa50
viswanthkj <viswanth1995@gmail.com>
2020-06-08 15:27:52 +0530

    Updates

---
+++ precache-manifest.3519bb153bc92bacdd010dab13cb91c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a62add03c7b1ee4602b4160412cfb80",
+    "url": "/monster-me/index.html"
+  },
+  {
+    "revision": "878b66a229a811eb2450",
+    "url": "/monster-me/static/css/main.6b5fc3a1.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/monster-me/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-me/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878b66a229a811eb2450",
+    "url": "/monster-me/static/js/main.e97c49bb.chunk.js"
+  },
+  {
+    "revision": "f20a1d1934f6d3e81e2f",
+    "url": "/monster-me/static/js/runtime-main.49a0d7eb.js"
+  }
+]);
